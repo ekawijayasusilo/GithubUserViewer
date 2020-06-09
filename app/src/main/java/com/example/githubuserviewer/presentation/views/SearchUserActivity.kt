69a5cfe7926f01557f457d993b8afd8b5a07ccd6
eka_wijaya <eka.wijaya@pegipegi.com>
2020-06-10 05:01:05 +0700

    Initialize project

---
+++ app/src/main/java/com/example/githubuserviewer/presentation/views/SearchUserActivity.kt
@@ -0,0 +1,15 @@
+package com.example.githubuserviewer.presentation.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.githubuserviewer.R
+
+
+class SearchUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_user)
+    }
+
+}
